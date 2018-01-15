0c9dc7f0e821c5a91044ce89827699440643779d
Julio Herrera <cesarchavarria0@gmail.com>
2018-01-15 14:35:28 -0600

    Adds a basic Express.js project with some configurations

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+var mongoose = require('mongoose')
+var config = require('./config/config')
+var app = express()
+
+// Database set up
+var mongoDB = 'mongodb://devherrera:postitapp@ds257627.mlab.com:57627/post-it'
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+})
+
+mongoose.Promise = global.Promise
+
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
+// App config setup
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Routes set up
+// const authentication = require('./routes/authentication')
+// const post = require('./routes/post')
+// const index = require('./routes/index')
+
+// app.use('/', index)
+// app.use('/user', authentication)
+// app.use('/post', post)
+
+// Running server
+app.listen(process.env.PORT || 3000)
+console.log('Server listening on port ' + config.port)
