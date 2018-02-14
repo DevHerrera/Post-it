63ef0ef4b491bae5c0a75def2dfa7df2226e1f4f
Julio Herrera <cesarchavarria0@gmail.com>
2018-02-14 15:34:32 -0600

    Minor Fixes

--- server/src/routes/post/postRoute.js
+++ server/src/routes/post/postRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const protected = require('../protected')
+const protected = require('../../middleware/protected')
 
 // Post controller
 const post_controller = require('../../controllers/post/postController')
